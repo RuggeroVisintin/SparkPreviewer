e9c731efd3fa4e68754ab163cafbeb06c3bb5860
Ruggero Visintin <ruggeahhnano@outlook.it>
2015-07-15 09:40:10 +0200

    Camera functions moved to Camera.js file, added ArcballCamera class

--- src/js/SparkPreviewer.js
+++ src/js/SparkPreviewer.js
@@ -1,17 +1,16 @@
-// The MIT License (MIT)
-
+// --------------------------------------------------------------------------------
 // Copyright (c) 2015 Ruggero Enrico Visintin
-
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,6 +18,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE. 
+// --------------------------------------------------------------------------------
 
 console.log("SparkViewer.js included");
 
@@ -32,6 +32,7 @@ JRV.include("core/RenderMesh.js");
 JRV.include("core/RenderModel.js");
 JRV.include("core/math/Vector3.js");
 JRV.include("core/input/PcInputManager.js");
+JRV.include("core/Camera.js");
 
 function Application(canvas) {
 	var mCanvas = canvas;
@@ -52,6 +53,8 @@ function Application(canvas) {
 	
 	var running;
 
+	var mArcballCamera;
+
 	var phi = 90 * Math.PI / 180;
 	var theta = 0 * Math.PI / 180;
 
@@ -60,13 +63,7 @@ function Application(canvas) {
 
 	var radius = 10;
 
-	var leftMouseDown = false;
-	var rightMouseDown = false;
-	var wheelMouseDown = false;
-	var oldMouseX;
-	var oldMouseY;
-
-	var LIT_VERTEX_SHADER_SOURCE =
+    var LIT_VERTEX_SHADER_SOURCE =
         "attribute vec3 position;" 								                     +
 		//"attribute vec3 color;" 								                     +
         //"attribute vec2 uv;" 									                     +
@@ -90,6 +87,9 @@ function Application(canvas) {
         renderer = new Renderer();
         renderer.initWebGL(mCanvas);
         
+                
+        mArcballCamera = new ArcballCamera();
+
         initBackground();
         initShaderPrograms();
         initDefaultModel();
@@ -100,6 +100,7 @@ function Application(canvas) {
 
         window.addEventListener('resize', onResizeEvent, false);
         inputManager = new PcInputManager(mCanvas);
+
     };
 
     this.run = function () {
@@ -218,21 +219,15 @@ function Application(canvas) {
 
 	var onResizeEvent = function () {
 	    console.log("Resizing");
-	    Matrix4.perspective(45, mCanvas.clientWidth / mCanvas.clientHeight, 0.1, 100, mProjectionMatrix);
+	    //Matrix4.perspective(45, mCanvas.clientWidth / mCanvas.clientHeight, 0.1, 100, mProjectionMatrix);
 	};
 
 	var updateRendering = function() 
 	{
 	    var mvp = Matrix4.create();
-	    var eyeX, eyeY, eyeZ;
+	    var camMatrices = mArcballCamera.updateMatrices();
 
-	    eyeX = radius * Math.sin(theta) * Math.sin(phi) + camTargetX;
-	    eyeY = radius * Math.cos(phi) + camTargetY;
-	    eyeZ = radius * Math.cos(theta) * Math.sin(phi) + 0;
-
-	    Matrix4.lookAt([eyeX, eyeY, eyeZ], [camTargetX, 0, 0], [0, 1, 0], mCameraView);
-
-	    Matrix4.multiply(mProjectionMatrix, mCameraView, mvp);
+	    Matrix4.multiply(camMatrices[0], camMatrices[1], mvp);
 	    Matrix4.multiply(mvp, mModelViewMatrix, mvp);
 
 	    var drawCall = new DrawCall();
@@ -257,17 +252,9 @@ function Application(canvas) {
 
 	        console.log(verticalDelta);
 
-	        if (verticalDelta > 0) {
-	            if ((phi * 180 / Math.PI) < 170) {
-	                phi += verticalDelta * 0.02;
-	            }
-	        } else if (verticalDelta < 0) {
-	            if ((phi * 180 / Math.PI) > 10) {
-	                phi += verticalDelta  * 0.02;
-	            }
-	        }
+	        mArcballCamera.rotateY(verticalDelta);
+	        mArcballCamera.rotateX(inputManager.getMouseHorizontalDelta())
 
-	        theta += inputManager.getMouseHorizontalDelta() * 0.02;
 	    }
 
 	    if (inputManager.getWheelDelta() != 0) {
@@ -276,15 +263,8 @@ function Application(canvas) {
 
 	        console.log(deltaRadius);
 
-	        if (deltaRadius > radius) {
-	            if (radius < 150) {
-	                radius += 0.5;
-	            }
-	        } else {
-	            if (radius > 0.5) {
-	                radius -= 0.5;
-	            }
-	        }
+	        mArcballCamera.moveRadius(deltaRadius);
+
 	    }
 
 	    inputManager.postUpdate();
--- src/js/core/Camera.js
+++ src/js/core/Camera.js
@@ -1,9 +1,81 @@
-﻿console.log("Camera.js included");
+﻿// --------------------------------------------------------------------------------
+// Copyright (c) 2015 Ruggero Enrico Visintin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. 
+// --------------------------------------------------------------------------------
 
-function ArcBallCamera() {
-    var mProjectionMatrix;
-    var mViewMatrix;
+console.log("Camera.js included");
+
+JRV.include("core/math/Matrix4.js");
+
+function ArcballCamera() {
+    var mPhi                = 90 * Math.PI / 180;
+    var mTheta              = 0 * Math.PI / 180;
     
-    this.rotate
+    // this is useless junk for now
+    // var mTargetX            = 0;  
+    // var mTargetY            = 0;
+
+    var mRadius             = 10;
+
+    var mProjectionMatrix   = Matrix4.create();
+    var mViewMatrix = Matrix4.create();
+
+    Matrix4.perspective(45, 800 / 600, 0.1, 100, mProjectionMatrix);
+
+    this.rotateX = function (deltaRads) {
+        mTheta += deltaRads * 0.02;
+    };
+
+    // i'll keep it locked till i'll solve matrices multiplication error
+    this.rotateY = function (deltaRads) {
+        if (deltaRads > 0) {
+            if ((mPhi * 180 / Math.PI) < 170) {
+                mPhi += deltaRads * 0.02;
+            }
+        } else if (deltaRads < 0) {
+            if ((mPhi * 180 / Math.PI) > 10) {
+                mPhi += deltaRads * 0.02;
+            }
+        }
+    };
+
+    this.moveRadius = function(deltaRadius) {
+        if (deltaRadius > mRadius) {
+            if (mRadius < 150) {
+                mRadius += 0.5;
+            }
+        } else {
+            if (mRadius > 0.5) {
+                mRadius -= 0.5;
+            }
+        }
+    };
+
+    this.updateMatrices = function () {
+        var eyeX = mRadius * Math.sin(mTheta) * Math.sin(mPhi);
+        var eyeY = mRadius * Math.cos(mPhi);
+        var eyeZ = mRadius * Math.cos(mTheta) * Math.sin(mPhi);
+
+        Matrix4.lookAt([eyeX, eyeY, eyeZ], [0, 0, 0], [0, 1, 0], mViewMatrix);
+
+        return [mProjectionMatrix, mViewMatrix];
+    };
 };
 
