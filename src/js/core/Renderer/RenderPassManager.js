120bb47a845a6a5bf6089109dc12844486086a93
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2015-08-19 21:31:06 +0200

    Update RenderPassManager.js

--- src/js/core/Renderer/RenderPassManager.js
+++ src/js/core/Renderer/RenderPassManager.js
@@ -1,5 +1,17 @@
-function RenderPassManager() {
-
-
-
+function RenderPassManager(renderer) {
+  var mRenderPasses   = [];
+  
+  return this;
 }
+
+RenderPassManager.prototype.addRenderPass = function(renderPass) {
+  mRenderPasses.push(renderPass);
+};
+
+RenderPassManager.prototype.render = function() {
+  for(var renderPass in mRenderPasses) {
+    renderPass.sort();
+    renderPass.render(renderer);
+    renderPass.clear();
+  }
+};
