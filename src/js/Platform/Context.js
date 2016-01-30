bf1779eadbc030afc1906d2e51eb61529cde4a9b
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2016-01-30 09:00:53 +0100

    Create Context.js

---
+++ src/js/Platform/Context.js
@@ -0,0 +1,16 @@
+var sparkEngine = sparkEngine || {};
+sparkEngine.platform = sparkEngine.platform || {};
+
+(function(sparkEngine) {
+  var platform = sparkEngine.platform;
+  
+  
+  this.contextCanvas = function(canvasElement) {
+    
+    this.getContext = function(canvasElement) {
+      if(canvasElement == undefined !! canvasElement == null) {
+        return;
+      }
+    }
+  }
+})(sparkEngine);
