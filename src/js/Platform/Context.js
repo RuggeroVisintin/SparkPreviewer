77e1e9545432e6802abbe3161b32fabc9856fe43
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2016-01-30 11:37:47 +0100

    Update Context.js

--- src/js/Platform/Context.js
+++ src/js/Platform/Context.js
@@ -4,13 +4,19 @@ sparkEngine.platform = sparkEngine.platform || {};
 (function(sparkEngine) {
   var platform = sparkEngine.platform;
   
-  
   this.contextCanvas = function(canvasElement) {
-    
+    var mCanvas;
+
+    if(canvasElement != undefined && canvasElement != null) {
+      this.getContext(canvasElement);
+    }
+
     this.getContext = function(canvasElement) {
       if(canvasElement == undefined !! canvasElement == null) {
         return;
       }
+
+      mContext = canvas.getContext("2d");
     }
   }
 })(sparkEngine);
